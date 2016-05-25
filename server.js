85e124f8cdc4eda02e55137e6041e8a6b95eb1ee
kiran paturi <kpaturi@smarttuition.com>
2016-05-25 14:34:31 +0530

    added code

---
+++ server.js
@@ -0,0 +1,20 @@
+var config = require('./config/env/development/development');
+var express = require('express');
+
+var checkUserName = function (req, res) {
+    if (req.param('name')) {
+        res.send("User name available");
+    } else {
+        res.send("Please Send the user name");
+    }
+};
+var returnData = function (req, res) {
+    res.send("hewllo Guys");
+}
+var app = express();
+app.get("/", checkUserName);
+app.get("/print", returnData);
+
+app.listen(config.port);
+
+console.log(config.port);
