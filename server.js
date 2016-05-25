b4f3d759aad2eb155c25d0a97c4fbb795de71b52
kiran paturi <kpaturi@smarttuition.com>
2016-05-25 14:53:14 +0530

    added dummy files

--- server.js
+++ server.js
@@ -8,13 +8,19 @@ var checkUserName = function (req, res) {
         res.send("Please Send the user name");
     }
 };
+
+
 var returnData = function (req, res) {
     res.send("hewllo Guys");
-}
+};
+
+var login = function(req,res){
+    
+};
 var app = express();
 app.get("/", checkUserName);
 app.get("/print", returnData);
+app.post("/login",login)
 
 app.listen(config.port);
-
 console.log(config.port);
